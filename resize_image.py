f77898edda22c8b06542a03cf0ba9a3c5b39cb7f
Crito02 <callen.crichton@gmail.com>
2022-07-14 12:01:06 +0800

    resize factor for failed imports

--- resize_image.py
+++ resize_image.py
@@ -6,8 +6,16 @@ from cv2 import dnn_superres
 sr = dnn_superres.DnnSuperResImpl_create()
 
 # Read image
-image = cv2.imread('./test_images/image2.jpeg')
+image = cv2.imread('./test_images/image4.jpg')
 
+scale_percent = 60
+
+#calculate the 50 percent of original dimensions
+width = int(image.shape[1] * scale_percent / 100)
+height = int(image.shape[0] * scale_percent / 100)
+# dsize
+dsize = (width, height)
+image = cv2.resize(image, dsize)
 # Read the desired model
 path = "model/EDSR_x4.pb"
 sr.readModel(path)
@@ -19,4 +27,4 @@ sr.setModel("edsr", 4)
 result = sr.upsample(image)
 
 # Save the image
-cv2.imwrite("./resized_images/upscaled2.jpeg", result)
+cv2.imwrite("./resized_images/upscaled4.jpg", result)
